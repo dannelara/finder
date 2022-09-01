d00b42a179ab90c6caa66a0f5abf98b4fcc79b86
Daniel <11daniel@live.se>
2022-09-01 13:53:51 +0200

    Header done

--- src/components/header.js/Header.js
+++ src/components/header.js/Header.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "../../utils";
-import { Nav, StyledHeader, SubBig, SubSmall, Ul } from "./Styles";
+import { Container, Nav, StyledHeader, SubBig, SubSmall, Ul } from "./Styles";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 export default function Header() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <StyledHeader>
       <SubBig>
@@ -23,6 +26,51 @@ export default function Header() {
               </Link>
             </Ul>
           </Nav>
+
+          <Container type="burger-menu">
+            {toggle ? (
+              <RiCloseLine
+                color="#D97979"
+                size={27}
+                onClick={() => setToggle(false)}
+              />
+            ) : (
+              <RiMenu3Line
+                color="#D97979"
+                size={27}
+                onClick={() => setToggle(true)}
+              />
+            )}
+
+            {toggle && (
+              <Container type="burger-menu-links">
+                <Link
+                  to="/"
+                  onClick={() => {
+                    setToggle(false);
+                  }}
+                >
+                  Home
+                </Link>
+                <Link
+                  to="/new"
+                  onClick={() => {
+                    setToggle(false);
+                  }}
+                >
+                  New Session
+                </Link>
+                <Link
+                  to="/finder"
+                  onClick={() => {
+                    setToggle(false);
+                  }}
+                >
+                  Finder
+                </Link>
+              </Container>
+            )}
+          </Container>
         </SubSmall>
       </SubBig>
     </StyledHeader>
--- src/components/header.js/Styles.js
+++ src/components/header.js/Styles.js
@@ -11,6 +11,36 @@ export const StyledHeader = styled.div`
   box-shadow: ${(props) => props.theme.header.boxshadow};
 `;
 
+export const Container = styled.div`
+  ${(props) =>
+    props.type === "burger-menu" && {
+      display: "none",
+      position: "relative",
+      "@media screen and (max-width: 600px)": {
+        display: "flex",
+      },
+    }}
+
+  ${(props) =>
+    props.type === "burger-menu-links" && {
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "flex-end",
+      flexDirection: "column",
+      textAlign: "end",
+      background: "#fff",
+      padding: "2rem",
+      position: "absolute",
+      right: 0,
+      top: "20px",
+      marginTop: ".5rem",
+      minWidth: "150px",
+      borderRadius: "5px",
+      boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.2)",
+      zIndex: 1000,
+    }}
+`;
+
 export const SubBig = styled.div`
   display: flex;
   align-items: center;
@@ -33,10 +63,12 @@ export const Nav = styled.nav`
   align-items: center;
   width: fit-content;
   height: 100%;
-  @media screen and (max-width: 600px) : {
+
+  @media (max-width: 600px) {
     display: none;
   }
 `;
+
 export const Ul = styled.ul`
   display: flex;
   justify-content: space-evenly;
